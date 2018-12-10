3402f9c6da45c4ea5060b021f3044d90580259ee
a1704537 <a1704537@myy.haaga-helia.fi>
2018-12-10 19:24:10 +0200

    added my project

---
+++ asetus.sh
@@ -0,0 +1,17 @@
+#bin/bash
+sudo apt-get update
+sleep 2s
+sudo ufw allow 8888
+sudo ufw deny 145/tcp
+sudo ufw deny 138/udp
+sudo ufw enable
+sleep 2s
+sudo apt-get install -y squid3
+sleep 5s
+sudo apt-get install -y squidguard 
+sleep 5s
+echo -e 'acl manager proto cache_object\nacl localhost src 127.0.0.1/32\nacl localhost src 127.0.0.1/24\nacl localhost src 192.168.1.197/32\nacl localhost src 192.168.1.197/24\nacl localhost src 192.168.1.196/32\nacl localhost src 192.168.1.196/24\nacl localhost src 10.0.0.0/24\nacl to_localhost dst 127.0.0.0/8\nacl localnet src 0.0.0.0/8 192.168.100.0/24 192.168.101.0/24 192.168.0.0/24 192.168.1.1/24\nacl SSL_ports port 443\nacl Safe_ports port 80 # http\nacl Safe_ports port 3128\nacl Safe_ports port 21 # ftp\nacl Safe_ports port 443 # https\nacl Safe_ports port 70 # gopher\nacl Safe_ports port 210 # wais\nacl Safe_ports port 1025-65535 # unregistered ports\nacl Safe_ports port 280 # http-mgmt\nacl Safe_ports port 488 # gss-http\nacl Safe_ports port 591 # filemaker\nacl Safe_ports port 777 # multiling http\nacl blocksites dstdomain "/etc/squid/BannedAds.squid"\nacl CONNECT method CONNECT\nhttp_access deny blocksites\nhttp_access allow to_localhost\nicp_access allow all\nhtcp_access allow all\nhttp_access allow all \nhttp_access allow all localnet\nhttp_access allow all localhost\nhttp_port 8888\n# Allow all machines to all sites\n '|sudo tee /etc/squid/squid.conf
+sleep 2s
+echo -e 'test.com'|sudo tee /etc/squid/BannedAds.squid
+sudo systemctl restart squid.service
+echo "Squid on nyt asennettu!"
--- squid.sls
+++ squid.sls
@@ -1,2 +1,12 @@
 squid3:
  pkg.installed
+
+squidguard:
+ pkg.installed
+
+/srv/salt/asetus:
+ cmd.run:
+   - source: salt://asetus.sh
+asetus.sh:
+ cmd.run:
+   - source: salt://asetus
