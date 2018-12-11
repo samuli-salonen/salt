d67fabaa2d4a14d5d9350b6d42b369b618503e4e
sam <a1704537@myy.haaga-helia.fi>
2018-12-11 18:52:17 +0200

    added more stuffff

--- lampsh.sh
+++ lampsh.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "CREATE DATABASE testi10;" | sudo mariadb -u root
-echo "GRANT ALL ON testi.* TO lamporja@localhost IDENTIFIED BY '1234';"$
 echo "Test database on tehty" | sudo tee /etc/salt/tehty.log
 
